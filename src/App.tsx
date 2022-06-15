f1c9542afac64eadd545592f0f572931c92ec689
iceboy1406 <ekodoko880@gmail.com>
2022-06-16 04:25:48 +0700

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-primary-500 text-4xl font-semibold">Hello World</h1>
+      <h1 className="text-primary-500 text-4xl font-semibold">Hello World</h1>
+    </>
+  )
+}
+
+export default App
