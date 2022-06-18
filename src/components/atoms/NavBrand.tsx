74fea4968ebc2f85b1efc7f7ef92b745aa70ffcb
iceboy1406 <ekodoko880@gmail.com>
2022-06-18 07:46:21 +0700

    edit credit for icon

--- src/components/atoms/NavBrand.tsx
+++ src/components/atoms/NavBrand.tsx
@@ -1,17 +1,13 @@
 const NavBrand = () => {
   return (
     <a
-      href="https://www.flaticon.com/free-icons/paper"
+      href="https://www.flaticon.com/free-icon/notes_858821"
       target={'_blank'}
       rel="noreferrer"
       className="block w-fit h-fit"
+      title={`Credit : https://www.flaticon.com/free-icon/notes_858821`}
     >
-      <img
-        src="/brand.svg"
-        alt="brand logo"
-        loading="lazy"
-        className="h-9"
-      />
+      <img src="/brand.svg" alt="brand logo" loading="lazy" className="h-9" />
     </a>
   )
 }
